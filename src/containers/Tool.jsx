b01d15da1b167b98f5f10c77abf2f5dce8e39c7c
ahmedabro <ahmedabro97@gmail.com>
2023-09-13 04:43:20 +0500

    Functionality implemented, All Done!

--- src/containers/Tool.jsx
+++ src/containers/Tool.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useRef, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { youtube_parser } from './utils'
 
 function Tool() {
+    const [inputData, setInputData] = useState("")
+    const inputElem = useRef()
+    const youtubeID = youtube_parser(inputData)
+    const [urlResult, setUrlResult] = useState("")
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(inputElem.current.value)
+        console.log(youtubeID)
+
+    
+        const options = {
+            method: 'GET',
+            url: 'https://youtube-mp36.p.rapidapi.com/dl',
+            params: {id: youtubeID},
+            headers: {
+              'X-RapidAPI-Key': import.meta.env.VITE_RAPID_YOUTUBE_MP3_KEY,
+              'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+            }
+          };
+
+          axios(options)
+            .then(resp => setUrlResult(resp.data.link))
+            .catch(err => console.log(err))
+
+        inputElem.current.value = ""
+        setInputData("");
+          
+          
+    }
+
+    const handleChange = () => {
+        setInputData(inputElem.current.value)
+    }
   return (
     <div className='tool-sec'>
       <Container>
@@ -10,11 +47,15 @@ function Tool() {
                 <div className='tool-content text-center'>
                     <h1>YouTube to MP3 Converter</h1>
                     <p>With this tool you can instantly convert your youtube videos into mp3</p>
-                    <form className='convert-form'>
-                        <input type="text" placeholder='Enter youtube link here...' />
+                    <form onSubmit={handleSubmit} className='convert-form'>
+                        <input ref={inputElem} value={inputData} onChange={handleChange} type="text" placeholder='Enter youtube link here...' />
                         <button type="submit">Convert</button>
                     </form>
-                    <a href="#">Download MP3</a>
+                    {
+                        urlResult 
+                        ? <a href={urlResult} target='_blank' rel='noreferrer'>Download MP3</a>
+                        : ''
+                    }
                 </div>
             </Col>
         </Row>
